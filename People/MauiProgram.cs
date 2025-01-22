f128ac2163c18d30993f28d9eabc7983c29ecbf4
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2025-01-22 12:57:38 -0500

    Commit 4: Integración de 'PersonRepository' a la interfaz de usuario.

--- People/MauiProgram.cs
+++ People/MauiProgram.cs
@@ -19,8 +19,9 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
-		// TODO: Add statements for adding PersonRepository as a singleton
+        string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+        builder.Services.AddSingleton<PersonRepository>(s => ActivatorUtilities.CreateInstance<PersonRepository>(s, dbPath));
 
-		return builder.Build();
+        return builder.Build();
 	}
 }
